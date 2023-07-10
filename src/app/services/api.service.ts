f9541cdc76b34192556aaa361f8ba300721e1490
GULBERK DEMIR <gulberk.demir@mercedes-benz.com>
2023-07-10 14:42:26 +0300

    navigation after editting ready

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {UserInterface} from "../types/user.model";
 import {BehaviorSubject, Observable, tap} from "rxjs";
+import {ActivatedRoute, Router} from "@angular/router";
 
 
 @Injectable({
@@ -12,7 +13,7 @@ export class ApiService {
   usersSub$ = new BehaviorSubject<UserInterface[]>([])
   apiURL = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
 
   createUser(user: UserInterface): Observable<UserInterface>{
     return this.http.post<UserInterface>("http://localhost:3000/userList/",user)
@@ -28,6 +29,7 @@ export class ApiService {
   }
 
   editUser(user: UserInterface, id: number): Observable<UserInterface>{
-    return this.http.put<UserInterface>("http://localhost:3000/userList/"+id, user);
+    return this.http.put<UserInterface>("http://localhost:3000/userList/"+id, user)
+      .pipe(tap(response => this.router.navigate(['dashboard/users'], {relativeTo: this.route})));
   }
 }
